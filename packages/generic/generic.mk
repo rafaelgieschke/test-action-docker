9f3b9cfc328d81c34d54ef55c1378d8f863f87f3
Rafael Gieschke <rafael@gieschke.de>
2019-05-22 20:36:59 +0000

    Allow login on tty1 (and ttyS0)

--- packages/generic/generic.mk
+++ packages/generic/generic.mk
@@ -4,6 +4,7 @@ GENERIC_SOURCE=
 define GENERIC_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D $(GENERIC_PKGDIR)/S99simple-cloud-init $(TARGET_DIR)/etc/init.d/
 	rm $(TARGET_DIR)/etc/init.d/S50avahi-daemon
+	echo '::respawn:/sbin/getty -L tty1 0' >> $(TARGET_DIR)/etc/inittab
 endef
 
 $(eval $(generic-package))

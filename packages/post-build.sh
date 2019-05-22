50bbc902518080841bf5eaaa0b93a6fb93123310
Rafael Gieschke <rafael@gieschke.de>
2019-05-22 17:04:43 +0000

    Enable console via serial port

--- packages/post-build.sh
+++ packages/post-build.sh
@@ -4,6 +4,6 @@ set -eu
 cp -f "$HOST_DIR/lib/grub/i386-pc/boot.img" "$BINARIES_DIR"
 
 cat > "$TARGET_DIR/boot/grub/grub.cfg" << EOF
-linux /boot/bzImage root=/dev/sda1 rootwait console=tty1
+linux /boot/bzImage root=/dev/sda1 rootwait console=tty1 console=ttyS0
 boot
 EOF

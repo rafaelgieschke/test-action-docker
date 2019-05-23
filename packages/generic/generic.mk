1c9a1df67c09c71b6583131ba11f391c9a5d2263
Rafael Gieschke <rafael@gieschke.de>
2019-05-23 12:56:55 +0000

    Fix stderr output on tty1
    & has to be escaped as \& inside the replacement string in sed's `s///`.

--- packages/generic/generic.mk
+++ packages/generic/generic.mk
@@ -5,7 +5,7 @@ define GENERIC_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D $(GENERIC_PKGDIR)/S99simple-cloud-init $(TARGET_DIR)/etc/init.d/
 	rm $(TARGET_DIR)/etc/init.d/S50avahi-daemon
 	echo '::respawn:/sbin/getty -L tty1 0' >> $(TARGET_DIR)/etc/inittab
-	sed -i '/rcS/ s#$$# 2>&1 | tee /dev/tty1#' $(TARGET_DIR)/etc/inittab
+	sed -i '/rcS/ s#$$# 2>\&1 | tee /dev/tty1#' $(TARGET_DIR)/etc/inittab
 endef
 
 $(eval $(generic-package))
